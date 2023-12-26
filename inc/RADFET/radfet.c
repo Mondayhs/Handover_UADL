aafce546e682e645cef93cbfc482a2332596e560
Cindy <cindy0901125897@gmail.com>
2023-12-26 11:52:36 +0800

    new: Control_R1 Control_R2 Control_EN

--- inc/RADFET/radfet.c
+++ inc/RADFET/radfet.c
@@ -12,6 +12,8 @@
  */
 void Radfet_Init(void){  //Initialize & Configure MSS GPIOs for RADFET & ADC
 	MSS_GPIO_config( MSS_GPIO_14 , MSS_GPIO_OUTPUT_MODE );	// RADFET_CTRL_
+	MSS_GPIO_config( MSS_GPIO_15 , MSS_GPIO_OUTPUT_MODE );
+	MSS_GPIO_config( MSS_GPIO_16 , MSS_GPIO_OUTPUT_MODE );
 
 }
 
@@ -21,6 +23,21 @@ void Radfet_Init(void){  //Initialize & Configure MSS GPIOs for RADFET & ADC
 void Control_R1(void){
     /* R1 */
 	MSS_GPIO_set_output(MSS_GPIO_14, 1);
+	MSS_GPIO_set_output(MSS_GPIO_15, 0);
+	MSS_GPIO_set_output(MSS_GPIO_16, 1);
+ }
 
+void Control_R2(void){
+    /* R2 */
+	MSS_GPIO_set_output(MSS_GPIO_14, 1);
+	MSS_GPIO_set_output(MSS_GPIO_15, 1);
+	MSS_GPIO_set_output(MSS_GPIO_16, 0);
+
+
+void Control_EN(void){
+	    /* R2 */
+	MSS_GPIO_set_output(MSS_GPIO_14, 1);
+	MSS_GPIO_set_output(MSS_GPIO_15, 1);
+	MSS_GPIO_set_output(MSS_GPIO_16, 1);
  }
 
