d94152eaabcb032550b8a48bd66797deb061ece9
hhhsien <alfredolover0921@gmail.com>
2024-01-22 18:22:06 +0800

    modify: Radfet_Init, Control_R1 new: Control_R2, Control_Zero

--- inc/RADFET/radfet.c
+++ inc/RADFET/radfet.c
@@ -1,7 +1,7 @@
 /*
  * radfet.c
  *
- *  Created on: 2023¦~12¤ë22¤é
+ *  Created on: 2023ï¿½~12ï¿½ï¿½22ï¿½ï¿½
  *      Author: OLDBOY
  */
 
@@ -10,10 +10,15 @@
 /*==============================================================================
 	RADFET Initial function
  */
-void Radfet_Init(void){  //Initialize & Configure MSS GPIOs for RADFET & ADC
-	MSS_GPIO_config( MSS_GPIO_14 , MSS_GPIO_OUTPUT_MODE );	// RADFET_CTRL_
 
+void Radfet_Init(void){  //Initialize & Configure MSS GPIOs for RADFET & ADC
+	MSS_GPIO_config( MSS_GPIO_14 , MSS_GPIO_OUTPUT_MODE );	// RADFET_CTRL_EN
+	MSS_GPIO_config( MSS_GPIO_15 , MSS_GPIO_OUTPUT_MODE );  //R2
+	MSS_GPIO_config( MSS_GPIO_16 , MSS_GPIO_OUTPUT_MODE );  //R1
+	MSS_GPIO_config( MSS_GPIO_1 , MSS_GPIO_OUTPUT_MODE );
+	MSS_GPIO_config( MSS_GPIO_30 , MSS_GPIO_INPUT_MODE );
 }
+
 
 /*==============================================================================
 	RADFET control function
@@ -21,6 +26,21 @@ void Radfet_Init(void){  //Initialize & Configure MSS GPIOs for RADFET & ADC
 void Control_R1(void){
     /* R1 */
 	MSS_GPIO_set_output(MSS_GPIO_14, 1);
+	MSS_GPIO_set_output(MSS_GPIO_15, 1);
+	MSS_GPIO_set_output(MSS_GPIO_16, 0);
+ }
 
+void Control_R2(void){
+    /* R2 */
+	MSS_GPIO_set_output(MSS_GPIO_14, 0);
+	MSS_GPIO_set_output(MSS_GPIO_15, 1);
+	MSS_GPIO_set_output(MSS_GPIO_16, 1);
+}
+
+void Control_Zero(void){
+	/* reset */
+	MSS_GPIO_set_output(MSS_GPIO_14, 0);
+	MSS_GPIO_set_output(MSS_GPIO_15, 0);
+	MSS_GPIO_set_output(MSS_GPIO_16, 0);
  }
 
