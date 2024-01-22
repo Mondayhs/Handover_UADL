a3bed00ac16b656dcb51895104e21b8cd24a0004
Cindy <cindy0901125897@gmail.com>
2024-01-22 10:13:53 +0800

    new command.h

--- inc/COMMAND/command.h
+++ inc/COMMAND/command.h
@@ -5,14 +5,14 @@
 //  *      Author: Cindy E14
 //  */
 
-// #ifndef COMMAND_COMMAND_H_
-// #define COMMAND_COMMAND_H_
-// #include "..\..\firmware\drivers\mss_uart\mss_uart.h"
-// #define MSS_UART_921600_BAUD    921600
+ #ifndef COMMAND_COMMAND_H_
+ #define COMMAND_COMMAND_H_
+ #include "..\..\firmware\drivers\mss_uart\mss_uart.h"
+ #define MSS_UART_921600_BAUD    921600
 
-// #include <stdio.h>
-// #include <stdint.h>
-// #include <stdlib.h>
-// #include <string.h>
+ #include <stdio.h>
+ #include <stdint.h>
+ #include <stdlib.h>
+ #include <string.h>
 
-// #endif /* COMMAND_COMMAND_H_ */
+ #endif /* COMMAND_COMMAND_H_ */
