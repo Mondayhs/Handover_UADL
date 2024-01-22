c38465976db8ce6ac310ee9cbece55793ea57722
hhhsien <146543648+hhhsien@users.noreply.github.com>
2024-01-22 10:08:02 +0800

    Update radfet.c
    R1 and R2

--- inc/RADFET/radfet.c
+++ inc/RADFET/radfet.c
@@ -1,7 +1,7 @@
 /*
  * radfet.c
  *
- *  Created on: 2023¦~12¤ë22¤é
+ *  Created on: 2023Â¦~12Â¤Ã«22Â¤Ã©
  *      Author: OLDBOY
  */
 
@@ -12,6 +12,8 @@
  */
 void Radfet_Init(void){  //Initialize & Configure MSS GPIOs for RADFET & ADC
 	MSS_GPIO_config( MSS_GPIO_14 , MSS_GPIO_OUTPUT_MODE );	// RADFET_CTRL_
+	MSS_GPIO_config( MSS_GPIO_15 , MSS_GPIO_OUTPUT_MODE );	// RADFET_CTRL_
+	MSS_GPIO_config( MSS_GPIO_16 , MSS_GPIO_OUTPUT_MODE );	// RADFET_CTRL_
 
 }
 
@@ -20,7 +22,22 @@ void Radfet_Init(void){  //Initialize & Configure MSS GPIOs for RADFET & ADC
  */
 void Control_R1(void){
     /* R1 */
-	MSS_GPIO_set_output(MSS_GPIO_14, 1);
+	MSS_GPIO_set_output(MSS_GPIO_14, 1);//CTRL_EN
+	MSS_GPIO_set_output(MSS_GPIO_15, 0);//CTRL_R2
+	MSS_GPIO_set_output(MSS_GPIO_16, 1);//CTRL_R1
 
  }
+ void Control_R2(void){
+    /* R2 */
+	MSS_GPIO_set_output(MSS_GPIO_14, 1);//CTRL_EN
+	MSS_GPIO_set_output(MSS_GPIO_15, 1);//CTRL_R2
+	MSS_GPIO_set_output(MSS_GPIO_16, 0);//CTRL_R1
 
+ }
+ void Control_Zero(void){
+    /* return to zero */
+	MSS_GPIO_set_output(MSS_GPIO_14, 0);//CTRL_EN
+	MSS_GPIO_set_output(MSS_GPIO_15, 0);//CTRL_R2
+	MSS_GPIO_set_output(MSS_GPIO_16, 0);//CTRL_R1
+
+ }
